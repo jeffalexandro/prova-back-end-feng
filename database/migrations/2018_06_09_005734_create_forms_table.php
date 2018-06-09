4fb46c6e1b281207e67fd207be0918443085189d
Jefferson <jeff_alexandro@hotmail.com>
2018-06-08 22:28:04 -0300

    Adicionar Migrations

---
+++ database/migrations/2018_06_09_005734_create_forms_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFormsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('category');
+            $table->integer('basePoints');
+            $table->dateTime('startDate');
+            $table->dateTime('endDate');
+            $table->boolean('isActive');
+            $table->boolean('alreadyAnswered');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forms');
+    }
+}
